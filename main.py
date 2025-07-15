c6ad436f32de5e08fc9f6d4f0ebd2be646271882
Dylan <dylantroche954@gmail.com>
2025-07-15 02:34:56 -0400

    Created ability to guess number

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
 
 
+
 print("Welcome to the number guessing game!")
+random_num = random.randint(1,100)
 
 def check_diffculty():
     user = input("please select a difficulty(easy/medium/hard): ")
@@ -19,9 +21,23 @@ def check_diffculty():
         case _:
             print("Difficulty selected is invalid")
 
-chances = check_diffculty()
+
+def select_number():
+    global chances
+    selected_num = int(input("Please select a number between 1 and 100"))
+    if selected_num == random_num:
+        print("that is correct, yippie :D")
+    elif random_num >= selected_num:
+        print(f"The number is greater than {selected_num}")
+        chances -= 1
+    else:
+        print(f"The number is less than {selected_num}")
+        chances -= 1
 
 
-random_num = random.randint(1,100)
 print(random_num)
+chances = check_diffculty()
+while chances > 0:
+    print(f"You have {chances} attemps remaining.")
+    select_number()
 
