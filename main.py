c0309a78e94276b64015aaf46741e7c25f9c4a0c
Dylan <dylantroche954@gmail.com>
2025-07-16 00:26:51 -0400

    Added ability to restart game, need to fix ability to quit game however

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 
 
 print("Welcome to the number guessing game!")
-random_num = random.randint(1,100)
 guess_correct = False
+is_playing = True
 
 def check_diffculty():
     print("Please select a difficulty level:")
@@ -26,7 +26,6 @@ def check_diffculty():
         case _:
             print("Difficulty selected is invalid, please select Easy, Medium or Hard")
 
-
 def check_range(number, start, stop):
     if number in range(start,stop):
         return "Within range"
@@ -34,35 +33,55 @@ def check_range(number, start, stop):
         return "Below range"
     else:
         return "Above range"
+    
+def restart_game(is_still_playing):
+    play_again = input("Would you like to begin a new round?").lower()
+    match play_again:
+        case "yes":
+            print("You will begin the next round!")
+            chances = check_diffculty()
+            select_number(chances,is_playing)
+            is_still_playing = True
+            return is_still_playing
+        case "no":
+            print("game is over")
+            is_still_playing = False
+            return is_still_playing
+        case _ :
+            print("not a valid command")
 
-def select_number(chances):
-    while chances > 0:
-        try:
-            print("")
-            print(f"You have {chances} attemps remaining.")
-            selected_num = int(input("Please select a number between 1 and 100: "))
-            value_range = check_range(selected_num, 1, 100)
-            match value_range:
-                case "Within range":
-                    if selected_num == random_num:
-                        print("that is correct, yippie :D")
-                        break #add loopable functionality
-                    elif random_num >= selected_num:
-                        print(f"The number is greater than {selected_num}")
-                        chances -= 1
-                    elif random_num <= selected_num:
-                        print(f"The number is less than {selected_num}")
-                        chances -= 1
-                case "Above range":
-                    print("This number is greater than 100, Try again")
-                    continue
-                case "Below range":
-                    print("This number is below than 100, Try again")
-                    continue
-        except ValueError:
-            print("Value selected is not a whole number, try again")
+def select_number(chances,start_playing):
+    random_num = random.randint(1,100)
+    print(random_num)
+    if start_playing == True:
+        while chances > 0:
+            try:
+                print("")
+                print(f"You have {chances} attemps remaining.")
+                selected_num = int(input("Please select a number between 1 and 100: "))
+                value_range = check_range(selected_num, 1, 100)
+                match value_range:
+                    case "Within range":
+                        if selected_num == random_num:
+                            print("that is correct, yippie :D")
+                            start_playing = restart_game(is_playing)
+                        elif random_num >= selected_num:
+                            print(f"The number is greater than {selected_num}")
+                            chances -= 1
+                        elif random_num <= selected_num:
+                            print(f"The number is less than {selected_num}")
+                            chances -= 1
+                    case "Above range":
+                        print("This number is greater than 100, Try again")
+                        continue
+                    case "Below range":
+                        print("This number is below than 100, Try again")
+                        continue
+            except ValueError:
+                print("Value selected is not a whole number, try again")
+    else:
+        pass
 
-print(random_num)
 chances = check_diffculty()
-select_number(chances)
+select_number(chances,is_playing)
 
