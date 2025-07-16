7e055e916856880c9c5ad70027c0e88f680f70c0
Dylan <dylantroche954@gmail.com>
2025-07-16 03:30:28 -0400

    Fixed formatting on strings

--- main.py
+++ main.py
@@ -15,19 +15,22 @@ def check_difficulty():
     user_diff = user.lower()
     match user_diff:
         case "easy":
+            print("")
             print("You have selected easy difficulty")
             return 10
         case "medium":
+            print("")
             print("You have selected medium difficulty")
             return 5
         case "hard":
+            print("")
             print("You have selected hard difficulty")
             return 3
         case _:
             print("Difficulty selected is invalid, please select Easy, Medium or Hard")
 
 def check_range(number, start, stop):
-    if number in range(start,stop):
+    if number in range(start,stop +1):
         return "Within range"
     elif number < start:
         return "Below range"
@@ -35,11 +38,11 @@ def check_range(number, start, stop):
         return "Above range"
     
 def restart_game():
-    print("WORKING")
     play_again = input("Would you like to begin a new round?").lower()
     match play_again:
         case "yes":
             print("You will begin the next round!")
+            print("")
             return True
         case "no":
             print("game is over")
@@ -59,12 +62,16 @@ def select_number(chances):
             match value_range:
                 case "Within range":
                     if selected_num == random_num:
+                        print("")
                         print("That is correct, yippie :D")
-                        return restart_game() #can return function
+                        print("")
+                        return restart_game()
                     elif random_num > selected_num:
+                        print("")
                         print(f"The number is greater than {selected_num}")
                         chances -= 1
                     elif random_num < selected_num:
+                        print("")
                         print(f"The number is less than {selected_num}")
                         chances -= 1
                 case "Above range":
